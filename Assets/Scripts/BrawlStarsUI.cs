b2eba9aefeeb6a66484a2998ebf2719e12937d33
sundayliu <sundayliu@aliyun.com>
2019-01-19 19:45:25 +0800

    Init project

---
+++ Assets/Scripts/BrawlStarsUI.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BrawlStarsUI : MonoBehaviour {
+
+    [SerializeField] private Text scoreLabel;
+	// Use this for initialization
+	void Start () {
+        scoreLabel.text = "Score:100";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnAddHP()
+    {
+        Debug.Log("On Add HP");
+    }
+}
