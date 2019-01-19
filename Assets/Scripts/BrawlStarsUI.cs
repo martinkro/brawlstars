aaf7e56e6154e6d5d4496c6bd9986eb03932b474
sundayliu <sundayliu@aliyun.com>
2019-01-19 22:22:44 +0800

    Canvas resolution

--- Assets/Scripts/BrawlStarsUI.cs
+++ Assets/Scripts/BrawlStarsUI.cs
@@ -7,11 +7,23 @@ public class BrawlStarsUI : MonoBehaviour {
 
     [SerializeField] private Text scoreLabel;
     public Button btnAddHP;
+
+    public void DumpResolutions()
+    {
+        Resolution[] resolutions = Screen.resolutions;
+        foreach(Resolution res in resolutions)
+        {
+            Debug.Log(string.Format("resolution:{0}x{1}",res.width,res.height));
+        }
+    }
 	// Use this for initialization
 	void Start () {
         btnAddHP.onClick.AddListener(OnAddHP);
         scoreLabel.text = "Score:100";
         Debug.Log("BrawlStarsUI.Start");
+        Debug.Log(string.Format("Screen Width:{0}", Screen.width));
+        Debug.Log(string.Format("Screen Height:{0}", Screen.height));
+        DumpResolutions();
     }
 	
 	// Update is called once per frame
